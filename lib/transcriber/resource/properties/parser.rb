2c607f3dc4dbcca9fe1928fd398503dc8dba2119
Victor Rodrigues <victorc.rodrigues@gmail.com>
2011-07-31 04:20:26 -0300

    property parsing with problems

--- lib/transcriber/resource/properties/parser.rb
+++ lib/transcriber/resource/properties/parser.rb
@@ -1,6 +1,7 @@
 class Transcriber::Resource::Property
   module Parser
     def parse(item, options = {})
+      puts "item: #{item}, options: #{options}, field: #{field}, serializer: #{serializer.inspect}"
       value = serializer.serialize(item[field])
       value = values.key(value) if values
       value

500e9b43326896855d6334ca38401032f3702c34
Victor Rodrigues <victorc.rodrigues@gmail.com>
2011-07-31 10:10:37 -0300

    solved

--- lib/transcriber.rb
+++ lib/transcriber.rb
@@ -1,7 +1,7 @@
 require 'date'
 require 'active_support/concern'
+require 'active_support/inflector'
 require 'active_support/core_ext/hash/slice'
-require 'active_support/core_ext/string/inflections'
 
 module Transcriber
   autoload :Resource, 'transcriber/resource'

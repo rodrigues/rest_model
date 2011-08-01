537f784d3652dae39f5cabdca248246364289818
Victor Rodrigues <victorc.rodrigues@gmail.com>
2011-07-31 23:33:34 -0300

    input path reloaded

--- lib/transcriber/resource/key/input_path.rb
+++ lib/transcriber/resource/key/input_path.rb
@@ -1,10 +1,18 @@
 class Transcriber::Resource::Key
   module InputPath
     def input_path(options = self.options)
-      return @input_path if @input_path
-      path = options.slice(:start_key, :field)
-      keys = path.empty? ? [name] : path.values.first.to_s.split('/')
-      @input_path = convert_input_keys.call(keys)
+      path = custom_path(options)
+      @input_path ||= convert_input_keys.call keys_for_path(path)
+    end
+
+    private
+
+    def custom_path(options)
+      options.slice(:start_key, :field)
+    end
+
+    def keys_for_path(path)
+      path.empty? ? [name] : path.values.first.to_s.split('/')
     end
   end
 end

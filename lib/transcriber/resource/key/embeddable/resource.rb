9a3c74d3715d1cd52c964bd1e53d749e7f568baf
Victor Rodrigues <victorc.rodrigues@gmail.com>
2011-08-01 04:20:21 -0300

    simplified Embeddable#to_resource

--- lib/transcriber/resource/key/embeddable/resource.rb
+++ lib/transcriber/resource/key/embeddable/resource.rb
@@ -2,11 +2,7 @@ class Transcriber::Resource::Embeddable
   module Resource
     def to_resource(parent)
       embedded = parent.__send__(name)
-
-      resource = embedded.kind_of?(Enumerable) ?
-                   embedded.map(&:resource)
-                 : embedded.resource
-
+      resource = one? ? embedded.resource : embedded.map(&:resource)
       {name => resource}
     end
   end

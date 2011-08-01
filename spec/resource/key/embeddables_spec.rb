453b316648bc0563c990085812d0e8a56f0e2f10
Guilherme Guimarães Filho <gguimaraesbr@gmail.com>
2011-08-01 19:40:48 -0300

    Refactoring specs

--- spec/resource/key/embeddables_spec.rb
+++ spec/resource/key/embeddables_spec.rb
@@ -2,21 +2,28 @@ require 'spec_helper'
 
 describe Transcriber::Resource::Embeddables do
   class Invoice < Transcriber::Resource
-    embeds_one  :contract,      some_option: 'what'
-    embeds_many :invoice_items, some_option: 'what'
+    embeds_one  :contract,      some_option: 'contract options'
+    embeds_many :invoice_items, some_option: 'invoice options'
   end
 
   describe ".embeds_one" do
     it "defines an attr_accessor with embeddable name" do
-      Invoice.new.tap do |i|
-        i.contract = 1
-        i.contract.should == 1
+      Invoice.new.tap do |invoice|
+        invoice.respond_to?("contract").should be_true
+        invoice.respond_to?("contract=").should be_true
       end
     end
 
     it "puts a new embeddable in keys list" do
-      Invoice.keys[0].name.should == :contract
-      Invoice.keys[0].options.should == {some_option: 'what', many: false}
+      Invoice.keys.find { |key| key.name == :contract }.should_not be_nil
+    end
+    
+    it "supports option definition" do
+      Invoice.keys.find { |key| key.name == :contract }.options[:some_option].should == 'contract options'
+    end
+    
+    it "really embeds one" do
+      Invoice.keys.find { |key| key.name == :contract }.options[:many].should be_false
     end
   end
 
@@ -29,8 +36,15 @@ describe Transcriber::Resource::Embeddables do
     end
 
     it "puts a new embeddable in keys list" do
-      Invoice.keys[1].name.should == :invoice_items
-      Invoice.keys[1].options.should == {some_option: 'what', many: true}
+      Invoice.keys.find { |key| key.name == :invoice_items }.should_not be_nil
+    end
+    
+    it "supports option definiton" do
+      Invoice.keys.find { |key| key.name == :invoice_items }.options[:some_option].should == 'invoice options'
+    end
+    
+    it "really embeds many" do
+      Invoice.keys.find { |key| key.name == :invoice_items }.options[:many].should be_true
     end
   end
 end
--- spec/resource/key/properties_spec.rb
+++ spec/resource/key/properties_spec.rb
@@ -2,27 +2,30 @@ require 'spec_helper'
 
 describe Transcriber::Resource::Properties do
   class Receipt < Transcriber::Resource
-    property :document_number, some_option: 'abab'
+    property :document_number, some_option: 'this is an option'
   end
   
   describe ".property" do
     it "defines an attr_accessor with property name" do
-      Receipt.new.tap do |r|
-        r.document_number = 2020
-        r.document_number.should == 2020
+      Receipt.new.tap do |receipt|
+        receipt.respond_to?("document_number").should be_true
+        receipt.respond_to?("document_number=").should be_true
       end
     end
-
+    
     it "puts a new property in keys list" do
-      Receipt.keys.first.name.should == :document_number
-      Receipt.keys.first.options.should == {some_option: 'abab'}
+      Receipt.keys.find { |key| key.name == :document_number }.should_not be_nil
+    end
+    
+    it "supports option definition" do
+      Receipt.keys.find { |key| key.name == :document_number }.options[:some_option].should == 'this is an option'
     end
   end
   
   describe ".properties" do
     class Item < Transcriber::Resource
       properties :id, :description
-      properties :price, :quantity, firstoption: 'this is the first option', secondoption: 'this is the second option'
+      properties :price, :quantity, first_option: 'this is the first option', second_option: 'this is the second option'
     end
     
     it "defines an attr_accessor for each given name" do
@@ -36,8 +39,8 @@ describe Transcriber::Resource::Properties do
     
     it "supports option definition" do
       Item.keys.find_all{ |item| [:price, :quantity].include?(item.name) }.each do |key|
-        key.options[:firstoption].should == 'this is the first option'
-        key.options[:secondoption].should == 'this is the second option'
+        key.options[:first_option].should == 'this is the first option'
+        key.options[:second_option].should == 'this is the second option'
       end
     end
   end

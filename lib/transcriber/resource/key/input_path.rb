f5626473564705dadce0f5bb146188e6bef01385
Victor Rodrigues <victorc.rodrigues@gmail.com>
2011-07-31 23:26:55 -0300

    big refactoring

--- lib/transcriber/resource/key/input_path.rb
+++ lib/transcriber/resource/key/input_path.rb
@@ -3,11 +3,8 @@ class Transcriber::Resource::Key
     def input_path(options = self.options)
       return @input_path if @input_path
       path = options.slice(:start_key, :field)
-      path = case path
-             when {}         then [name]
-             when Hash       then path.values.first.to_s.split('/')
-             end
-      @input_path = convert_input_keys.call(path)
+      keys = path.empty? ? [name] : path.values.first.to_s.split('/')
+      @input_path = convert_input_keys.call(keys)
     end
   end
 end

88d706e64a6d057e22e0c5ed2d114a22e98a9ed5
Victor Rodrigues <victorc.rodrigues@gmail.com>
2011-08-01 20:19:36 -0300

    embeddable parser spec

---
+++ spec/resource/key/embeddable/parser_spec.rb
@@ -0,0 +1,39 @@
+require 'spec_helper'
+
+describe Transcriber::Resource::Embeddable::Parser do
+
+  context "when it embeds one" do
+    before do
+      class ExampleChild < Transcriber::Resource
+        property :id
+      end
+    end
+
+    let(:value) {{"id" => "7000"}}
+
+    subject {Transcriber::Resource::Embeddable.new(:example_child, many: false)}
+
+    it "parses child" do
+      child = subject.parse(value)
+      child.id.should == "7000"
+    end
+  end
+
+  context "when it embeds many" do
+    before do
+      class ExampleChild < Transcriber::Resource
+        property :id
+      end
+    end
+
+    let(:value) {[{"id" => "7000"}, {"id" => "7001"}]}
+
+    subject {Transcriber::Resource::Embeddable.new(:example_children, many: true)}
+
+    it "parses children" do
+      children = subject.parse(value)
+      children[0].id.should == "7000"
+      children[1].id.should == "7001"
+    end
+  end
+end

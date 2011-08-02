0bbe59c747703771a8796671c181786f8b0a9b27
Victor Rodrigues <victorc.rodrigues@gmail.com>
2011-08-02 15:47:26 -0300

    big refactoring

--- lib/transcriber/resource/builder.rb
+++ lib/transcriber/resource/builder.rb
@@ -1,11 +1,10 @@
 class Transcriber::Resource
   module Builder
-    autoload :Relations,    'transcriber/resource/builder/relations'
-    autoload :Embeddables,  'transcriber/resource/builder/embeddables'
-    autoload :Properties,   'transcriber/resource/builder/properties'
-
-    include Relations
-    include Embeddables
-    include Properties
+    autoload :Relations,   'transcriber/resource/builder/relations'
+    autoload :Embeddables, 'transcriber/resource/builder/embeddables'
+    autoload :Properties,  'transcriber/resource/builder/properties'
+    include   Relations
+    include   Embeddables
+    include   Properties
   end
 end

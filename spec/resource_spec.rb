d06252eb0e889380e95af0cee8debd96384ac85e
Victor Rodrigues <victorc.rodrigues@gmail.com>
2011-08-02 22:25:07 -0300

    toc

--- spec/resource_spec.rb
+++ spec/resource_spec.rb
@@ -13,7 +13,7 @@ describe Resource do
 
     it "returns a hash with properties and its values" do
       example = Example.new(login: 'jackiechan2010', name: 'Jackie Chan', age: 45)
-      example.resource.should ==    {login: 'jackiechan2010', name: 'Jackie Chan', age: 45}
+      example.resource.should == {login: 'jackiechan2010', name: 'Jackie Chan', age: 45}
     end
   end
 

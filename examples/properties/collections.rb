0f58a04342be5617a1580533f3840c0b75df4b54
Victor Cavalcanti <victorc.rodrigues@gmail.com>
2011-08-15 19:38:16 -0300

    parsing a collection of resources

---
+++ examples/properties/collections.rb
@@ -0,0 +1,12 @@
+$:.push 'lib'; require 'transcriber'
+
+class Customer < Transcriber::Resource
+  property :login
+end
+
+@customers = Customer.parse([{"login" => "jackiechan2010"},
+                             {"login" => "brucelee"},
+                             {"login" => "vcr2"}])
+
+puts "root:     #{@customers.inspect}"
+puts "resource: #{Customer.resources @customers}"

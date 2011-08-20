ea08ed032e08ff50b4f5f285326e68f494a7c4e4
Victor Cavalcanti <victorc.rodrigues@gmail.com>
2011-08-20 08:39:02 -0300

    invisible testing

--- examples/embeds_many/simple.rb
+++ examples/embeds_many/simple.rb
@@ -1,14 +1,14 @@
 $:.push 'lib'; require 'transcriber'
 
 class Item < Transcriber::Resource
-  property :id
+  property :id, type: Integer
 end
 
 class Root < Transcriber::Resource
   embeds_many :items
 end
 
-@root = Root.parse({"items" => [{"id" => 2000}]}).first
+@root = Root.parse({"items" => [{"id" => 2000}, {"id" => 2001}]}).first
 
 puts "root:     #{@root.inspect}"
 puts "resource: #{@root.resource}"
--- spec/integration/embeds_many_spec.rb
+++ spec/integration/embeds_many_spec.rb
@@ -1,5 +1,32 @@
 require 'spec_helper'
 
 describe_example 'embeds_many/simple' do
+  it 'parses first embedded item' do
+    root.items[0].id.should == 2000
+  end
 
+  it 'parses second embedded item' do
+    root.items[1].id.should == 2001
+  end
+end
+
+
+describe_example 'embeds_many/invisible' do
+  it 'parses invisible item correctly' do
+    root.locale.should == 'pt-BR'
+  end
+
+  it "doesn't show embedded items in resource" do
+    root.resource.should_not have_key('locale')
+  end
+end
+
+describe_example 'embeds_many/with_class_name' do
+
+end
+
+describe_example 'embeds_many/with_if' do
+end
+
+describe_example 'embeds_many/with_start_key' do
 end

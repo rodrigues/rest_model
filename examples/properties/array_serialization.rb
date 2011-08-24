61c2015e6b6aa8f2c27242b7f574852a7efc2041
Victor Cavalcanti <victorc.rodrigues@gmail.com>
2011-08-24 19:48:14 -0300

    enumerable serialization

---
+++ examples/properties/array_serialization.rb
@@ -0,0 +1,10 @@
+$:.push 'lib'; require 'transcriber'
+
+class Customer < Transcriber::Resource
+  property :products, type: Enumerable
+end
+
+@root = Customer.parse({'products' => ['a', 'b', 'c']}).first
+
+puts "root:     #{@root.inspect}"
+puts "resource: #{@root.resource}"

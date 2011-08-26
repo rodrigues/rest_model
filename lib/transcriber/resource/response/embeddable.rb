ff0583d5597badf08820f6f55bf3ee02af8d0176
Victor Cavalcanti <victorc.rodrigues@gmail.com>
2011-08-26 19:12:06 -0300

    bugfix: embeddable was failing when it was nil

--- lib/transcriber/resource/response/embeddable.rb
+++ lib/transcriber/resource/response/embeddable.rb
@@ -4,8 +4,15 @@ module Transcriber
       module Embeddable
         def to_resource(parent)
           return {} unless visible?(parent)
-          embedded = parent.__send__(name)
-          resource = one? ? embedded.resource : embedded.map(&:resource)
+
+          resource = nil
+
+          if embedded = parent.__send__(name)
+            resource = one? ? embedded.resource : embedded.map(&:resource)
+          elsif many?
+            resource = []
+          end
+
           {name => resource}
         end
       end

cb38cea44ff10afad91baeb65e2baaa5d046e816
Victor Cavalcanti <victorc.rodrigues@gmail.com>
2011-09-10 23:27:07 -0300

    cooler

--- lib/transcriber/resource/key/property.rb
+++ lib/transcriber/resource/key/property.rb
@@ -4,14 +4,15 @@ module Transcriber
       include Parser::Property
       include Response::Property
 
-      attr_accessor :serializer
-      attr_accessor :translations
+      attr_accessor :serializer, :translations
 
       def initialize(name, options = {})
         super
         @serializer   = options.fetch(:type, Serialization::String)
-        @translations = options[:values]        if options[:values].kind_of?(Proc)
-        @translations = options[:values].invert if options[:values].kind_of?(Hash)
+        @translations = case values = options[:values]
+                        when Proc then values
+                        when Hash then values.invert
+                        end
       end
 
       def from_hash(value)

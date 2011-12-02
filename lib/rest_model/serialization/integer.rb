c97c6261f59e943ee047c2374849f2a8328bd3ac
Victor Cavalcanti <victorc.rodrigues@gmail.com>
2011-12-02 19:41:54 -0200

    integer serialization with lasers

--- lib/rest_model/serialization/integer.rb
+++ lib/rest_model/serialization/integer.rb
@@ -2,15 +2,22 @@ class RestModel
   module Serialization
     class Integer
       def self.serialize(value)
-        Integer(value)
+        case value
+        when String
+          if value =~ /^\d+$/
+            value.to_i
+          else
+            raise
+          end
+        else
+          Integer(value)
+        end
       rescue
         raise SerializationError, "value '#{value}' is not an integer"
       end
 
       def self.desserialize(value)
-        Integer(value)
-      rescue
-        raise SerializationError, "value '#{value}' is not an integer"
+        serialize(value)
       end
     end
   end
